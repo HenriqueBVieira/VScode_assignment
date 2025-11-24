2e77f352d2f8256f66062648b8080e6851a3a509
HenriqueBVieira <hbvieira@usf.edu>
2025-11-23 23:23:15 -0500

    Fixed small code error

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-print("Welcome to my girst VS Code program!") # Welcome message
+print("Welcome to my first VS Code program!") # Welcome message
 hours = input("How many hours did you exercise today? ") #Input
-hours = int(hours) #Converts to int
-avg_calories_burnt = hours*300 #Calculation using an avg number of cal
-print(f"You have burnt {avg_calories_burnt} calories on average today! Well done!")
 try: #Error handling
     hours = int(hours)
 except ValueError:
     print("Please enter a valid number.")
-    exit()
+    raise SystemExit
+avg_calories_burnt = hours*300 #Calculation using an avg number of cal
+print(f"You have burnt {avg_calories_burnt} calories on average today! Well done!")
+
 
 
