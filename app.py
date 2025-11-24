5627884981cc402c35f9cb6d76df8bfbc218a21b
HenriqueBVieira <hbvieira@usf.edu>
2025-11-23 22:57:41 -0500

    Displayed the results of calculation

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ print("Welcome to my girst VS Code program!")
 hours = input("How many hours did you exercise today? ")
 hours = int(hours)
 avg_calories_burnt = hours*300
+print(f"You have burnt {avg_calories_burnt} calories on average today! Well done!")
 
