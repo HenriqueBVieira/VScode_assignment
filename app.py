146c65392dd28c55a461946d9f6b51df9258858a
HenriqueBVieira <hbvieira@usf.edu>
2025-11-23 22:58:47 -0500

    Added simple error handling in case user inputs a non int value

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ hours = input("How many hours did you exercise today? ")
 hours = int(hours)
 avg_calories_burnt = hours*300
 print(f"You have burnt {avg_calories_burnt} calories on average today! Well done!")
+try:
+    hours = int(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
 
