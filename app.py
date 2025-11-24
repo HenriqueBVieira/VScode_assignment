68502a30c744bc7fcedd3f96b14e26b4afd9ad91
HenriqueBVieira <hbvieira@usf.edu>
2025-11-23 23:02:19 -0500

    added comments

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-print("Welcome to my girst VS Code program!")
-hours = input("How many hours did you exercise today? ")
-hours = int(hours)
-avg_calories_burnt = hours*300
+print("Welcome to my girst VS Code program!") # Welcome message
+hours = input("How many hours did you exercise today? ") #Input
+hours = int(hours) #Converts to int
+avg_calories_burnt = hours*300 #Calculation using an avg number of cal
 print(f"You have burnt {avg_calories_burnt} calories on average today! Well done!")
-try:
+try: #Error handling
     hours = int(hours)
 except ValueError:
     print("Please enter a valid number.")
     exit()
+
 
